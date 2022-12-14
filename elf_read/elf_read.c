70148709576c545ad228870f1663f2dd25360873
tom7438 <tom192010@live.fr>
2022-12-14 13:41:58 +0100

    Modif fichier "programme principal"

--- elf_read/elf_read.c
+++ elf_read/elf_read.c
@@ -10,7 +10,7 @@
 void usage(char *name) {
     fprintf(stderr, "Usage:\n"
         "%s [ --help ] [ --debug file ] [ -h | -S | -s ] file\n\n"
-        "Prints values given as option. The --debug flag enables the output produced by "
+        "Prints information about the given ELF file. The --debug flag enables the output produced by "
         "calls to the debug function in the named source file.\n"
         , name);
 }
