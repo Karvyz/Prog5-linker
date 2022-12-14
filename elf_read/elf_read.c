1c9399101a02a2f1d56a12270f0e58d8de174e83
tom7438 <tom192010@live.fr>
2022-12-14 14:04:50 +0100

    Modif Makefile avec prog principal

--- elf_read/elf_read.c
+++ elf_read/elf_read.c
@@ -14,3 +14,29 @@ void usage(char *name) {
         "calls to the debug function in the named source file.\n"
         , name);
 }
+
+int main(int argc, char *argv[]) {
+    int opt;
+    char *file_name;
+    FILE *file;
+    Elf32_Ehdr header;
+    Elf32_Shdr *sections;
+    Elf32_Shdr *section;
+    Elf32_Shdr *section_names;
+    char *section_names_data;
+    int i;
+
+    struct option longopts[] = {
+            {"header",      no_argument,       NULL, 'h'},
+            {"sections",    no_argument,       NULL, 'S'},
+            {"section",     required_argument, NULL, 'x'},
+            {"symbols",     no_argument,       NULL, 's'},
+            {"relocations", no_argument,       NULL, 'r'},
+            {"help",        no_argument,       NULL, 'H'},
+            {NULL,          0,                 NULL, 0}
+    };
+
+    file_name = NULL;
+
+    return 0;
+}
