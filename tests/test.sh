3a6905c879f9bdde710e46c305200a0996220eec
Henri Gros <henrigros1@gmail.com>
2023-01-03 15:24:43 +0100

    test over all files

--- tests/test.sh
+++ tests/test.sh
@@ -2,15 +2,16 @@
 
 cmp_elf() {
     ./read_elf $1 $2 > tmp1
-    readelf $1 $2 > tmp2
+    arm-none-eabi-readelf $1 $2 > tmp2
     if ! cmp -s tmp1 tmp2; then
-        printf 'Test fail with the argument "%s"\n' "$1"
+        printf 'Test fail on file "%s" with the argument "%s"\n' "$file" "$1"
     fi
     rm tmp1 tmp2
 }
 
 
-cmp_elf -h $1
-cmp_elf -S $1
-cmp_elf -s $1
-cmp_elf -r $1
+for file in Exemples_ELF32_Files/*.o
+do
+    cmp_elf -h $file
+    cmp_elf -s $file
+done
