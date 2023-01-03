ec9fc8ef2af4c18bbf3e1d8b375ef8a61678af05
tom7438 <tom192010@live.fr>
2023-01-03 14:08:37 +0100

    Modif fonction Etape 6

--- elf_lib/elf_lib.h
+++ elf_lib/elf_lib.h
@@ -124,9 +124,13 @@ void print_relocations(FILE *fout);
  * @brief   Fusion et numérotation des sections
  *          du fichier ELF
  *
- * @param   f1 flux
- * @param   f2 flux
+ * @param   f1 flux 1 (fichier qui sera modifié)
+ * @param   f2 flux 2 (fichier qui sera ajouté)
+ * @param   header1 en-tête du fichier 1
+ * @param   sections1 table des sections du fichier 1
+ * @param   header2 en-tête du fichier 2
+ * @param   sections2 table des sections du fichier 2
  */
-void fusion_sections(FILE *f1, FILE *f2);
+void fusion_sections(FILE *f1, FILE *f2, Elf32_Ehdr header1, Elf32_Shdr sections1, Elf32_Ehdr header2, Elf32_Shdr sections2);
 
 #endif
