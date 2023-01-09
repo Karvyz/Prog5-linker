60b25c8dfa734f4580df62892c1ed11609243688
tibo <thibault.poncon@gmail.com>
2023-01-09 11:55:35 +0100

    creation des boucles principales

--- elf_lib/fusion_reloc.c
+++ elf_lib/fusion_reloc.c
@@ -9,23 +9,38 @@
 void fusion_relocation(FILE *f1, FILE *f2, FILE *result, Elf32_Ehdr elf_h1, Elf32_Shdr* elf_SH1, Elf32_Sym *elf_Sym1, Elf32_Ehdr elf_h2, Elf32_Shdr* elf_SH2, Elf32_Sym *elf_Sym2){
     //initialisation des variables
     
-    //parcours des sections de f1 et verification si le type de section de f1 est relocation
+                
+                
+    //parcours des sections de f1 
+    for (int i = 0; i < elf_h1.e_shnum; i++) {
+        //verification si le type de section de f1 est relocation
+        if (elf_SH1[i].sh_type == SHT_REL){
 
-        //parcours et verification si le type de section de f2 est relocation 
-        //et comparaison avec les nom dans f2, si oui:
+            //Recuperation du nom de s1:
+            char *name_section1 = read_from_shstrtab(elf_SH1[i].sh_name);
+            //parcours de f2:
+            for (int j; j < elf_h2.e_shnum; j++){
+                //recuperation du nom de s2:
+                char *name_section2 = read_from_shstrtab(elf_SH2[j].sh_name);
+                //verification si le type de section de f2 est relocation et comparaison avec les nom dans f2
+                if (elf_SH2[j].sh_type == SHT_REL && name_section1 == name_section2){
+                    //concatenation r1 et r2
 
-            //concatenation r1 et r2
+                }
+                free(name_section2)
 
-        //si non :
+            }
+            //si non :
 
-            //ajout de r1 a la suite
+                //ajout de r1 a la suite
             
 
     //ajout des r2 non fait à la suite
             
         
         
-
-        
+            free(name_section1);
+        }
+    }
 
 }
