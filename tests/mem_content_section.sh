96902c9a8d4dbf7a5ee295258d218acc4f748ad2
Henri Gros <henrigros1@gmail.com>
2023-01-10 16:17:36 +0100

    minor fixes for tests

--- tests/mem_content_section.sh
+++ tests/mem_content_section.sh
@@ -3,7 +3,7 @@ for file in Exemples_ELF32_Files/*.o
 do
     valgrind -q --leak-check=full --show-leak-kinds=all ./read_elf -x 1 $file 1> tmp1 2> error.txt
     if [ -s error.txt ]; then
-        echo error on $file on section $num_section
+        echo Memory leak on file $file with option -h 1
         rm tmp1 error.txt
         exit 1
     fi
