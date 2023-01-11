b6f9b8afad1b7c2e9a7be8a9fd61975ca5cf8000
tibo <thibault.poncon@gmail.com>
2023-01-11 10:56:57 +0100

    ajout d'information dans les fichier .h

--- elf_lib/elf_write.h
+++ elf_lib/elf_write.h
@@ -12,10 +12,32 @@
 #include <stdio.h>
 #include <elf.h>
 
+
+/**
+ * @brief   ecriture du header dans le fichier de sortie
+ *
+ * @param   f fichier de sortie
+ * @param   elf_h header
+ */
 void write_header(FILE *f, Elf32_Ehdr elf_h);
 
+/**
+ * @brief   ecriture de la table des section dans le fichier de sortie
+ *
+ * @param   f fichier de sortie
+ * @param   elf_h header
+ * @param   arr_elf_SH table des sections
+ */
 void write_sections(FILE *f, Elf32_Ehdr elf_h, Elf32_Shdr *arr_elf_SH);
 
+
+/**
+ * @brief   gestion de l'ecriture des structures de fusion dans un fichier de sortie
+ *          (actuellement prend un fichier .o, le passe en structure puis reécrit un fichier .o)
+ *
+ * @param   elf_h header
+ * @param   arr_elf_SH table des sections
+ */
 void write_main(Elf32_Ehdr elf_h, Elf32_Shdr *arr_elf_SH);
 
 #endif
--- elf_lib/fusion_reloc.h
+++ elf_lib/fusion_reloc.h
@@ -4,11 +4,43 @@
 #include <elf.h>
 #include <stdio.h>
 
+
+
+Elf32_Addr new_offset_reloc(Elf32_Addr old_offset);
+
+
+/**
+ * @brief   calcul le r_info en fonction de la nouvelle table de symbole
+ *
+ * @param   r_info contenue de le fichier d'entrer
+ * 
+ * @return le r_info calculer   
+ */
+Elf32_Word new_info_reloc(Elf32_Word old_info);
+
+
 /**
  * @brief   
  *
- * @param   
- * @param   
+ * @param   elf_SH sections
+ * @param   f    fichier d'entrer (f1 ou f2)
+ * @param   result fichier de sortie
+ */
+void lecture_ecriture_reloc(Elf32_Shdr elf_SH, FILE *f, FILE *result);
+
+
+/**
+ * @brief   
+ *
+ * @param   f1 fichier d'entre 1
+ * @param   f2 fichier d'entre 2
+ * @param   result ficheir de sortie
+ * @param   elf_h1 header 1
+ * @param   elf_SH1 sections 1
+ * @param   elf_sym1 symbole 1
+ * @param   elf_h2 header 2
+ * @param   elf_SH2 sections 2
+ * @param   elf_sym2 symbole 2
  */
 void fusion_relocation(FILE *f1, FILE *f2, FILE *result, Elf32_Ehdr elf_h1, Elf32_Shdr* elf_SH1, Elf32_Sym *elf_Sym1, Elf32_Ehdr elf_h2, Elf32_Shdr* elf_SH2, Elf32_Sym *elf_Sym2);
 
